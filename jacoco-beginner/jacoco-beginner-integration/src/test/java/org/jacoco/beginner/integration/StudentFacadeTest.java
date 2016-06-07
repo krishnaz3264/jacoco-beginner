e3b5ca0d260e3350093d64a44b13d2d572e01389
krishna <krishnachaitanya3264@yahoo.com>
2016-06-08 00:05:09 +0530

    added jersey test support

--- jacoco-beginner/jacoco-beginner-integration/src/test/java/org/jacoco/beginner/integration/StudentFacadeTest.java
+++ jacoco-beginner/jacoco-beginner-integration/src/test/java/org/jacoco/beginner/integration/StudentFacadeTest.java
@@ -1,11 +1,20 @@
 package org.jacoco.beginner.integration;
 
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
 import org.jacoco.beginner.impl.Student;
 import org.jacoco.beginner.ws.StudentFacade;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class StudentFacadeTest {
+public class StudentFacadeTest extends JerseyTest {
+
+	@Override
+	protected Application configure() {
+		return new ResourceConfig();
+	}
 
 	private StudentFacade facade = new StudentFacade();
 
